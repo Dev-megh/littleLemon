75e194968779320a10ababaaba625e3b1b1553d1
Dev-megh <chevlimegh@gmail.com>
2023-12-09 10:28:33 +0530

    Added routes

--- littleLemon/restaurant/views.py
+++ littleLemon/restaurant/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics 
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 from django.shortcuts import render
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 # Create your views here.
 def index(request):
 	return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
+	permission_classes = [IsAuthenticated] 
